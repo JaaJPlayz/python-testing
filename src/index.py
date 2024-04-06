2e72c640e246880eb6120960b9b56db85c1f4111
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-04-06 09:27:45 -0300

    Binary Search impl

--- src/index.py
+++ src/index.py
@@ -1,30 +1,64 @@
 from rich.console import Console
 import rich.traceback
-from typing import List
 
 rich.traceback.install()
 console = Console()
 
+data = {
+    "Mathew": {
+        "id": 1,
+        "math": 7.5,
+        "english": 8.5,
+        "history": 9.5,
+        "science": 10.0,
+        "geography": 9.5,
+        "politics": 9.0,
+        "art": 8.0,
+        "music": 9.0,
+        "sport": 9.0,
+    },
+    "Sarah": {
+        "id": 2,
+        "math": 10,
+        "english": 7.5,
+        "history": 6.5,
+        "science": 5.6,
+        "geography": 10,
+        "politics": 6.3,
+        "art": 8.0,
+        "music": 10,
+        "sport": 10,
+    },
+}
 
-def fib(iter: int) -> List[int]:
-    """Calculate the first n Fibonacci numbers"""
-    if iter <= 0:
-        raise ValueError("Invalid iteration amount, must be > 0")
-    if iter < 2:
-        return [1]
-    if iter == 2:
-        return [1, 1]
 
-    fibs = [1, 1]
-    for _ in range(2, iter):
-        fibs.append(fibs[-1] + fibs[-2])
+class BinarySearch:
+    """Binary search implementation"""
+    def __init__(self, data):
+        self.data = data
+        self.keys = list(data.keys())
 
-    return fibs
+    """Binary search"""
+    def search(self, target):
+        left, right = 0, len(self.keys) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if self.keys[middle] == target:
+                return self.data[target]
+            elif self.keys[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return None
 
+    """Search subject"""
+    def search_subject(self, person, subject):
+        return self.data.get(person, {}).get(subject)
 
 def main():
-    console.print(fib(-1))
-
+    # Example usage
+    search_result = BinarySearch(data).search_subject("Sarah", "english")
+    console.print(search_result)
 
 if __name__ == "__main__":
     main()
